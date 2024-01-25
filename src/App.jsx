6b1ba1f43dcab013078541502ef1ce6870d54657
CodeSandbox <support@codesandbox.io>
2024-01-25 05:46:16 +0000

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,20 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Counter from './components/Counter/Counter.jsx';
-import Header from './components/Header.jsx';
-import { log } from './log.js';
+import Counter from "./components/Counter/Counter.jsx";
+import Header from "./components/Header.jsx";
+import { log } from "./log.js";
+import ConfigureCounter from "./components/Counter/ConfigureCounter.jsx";
 
 function App() {
-  log('<App /> rendered');
-
-  const [enteredNumber, setEnteredNumber] = useState(0);
+  log("<App /> rendered");
   const [chosenCount, setChosenCount] = useState(0);
-
-  function handleChange(event) {
-    setEnteredNumber(+event.target.value);
-  }
-
-  function handleSetClick() {
-    setChosenCount(enteredNumber);
-    setEnteredNumber(0);
-  }
 
   return (
     <>
       <Header />
       <main>
-        <section id="configure-counter">
-          <h2>Set Counter</h2>
-          <input type="number" onChange={handleChange} value={enteredNumber} />
-          <button onClick={handleSetClick}>Set</button>
-        </section>
-        <Counter initialCount={chosenCount} />
+        <ConfigureCounter onSetChosenCount={setChosenCount} />
+        <Counter key={chosenCount} initialCount={chosenCount} />
       </main>
     </>
   );
--- src/components/UI/IconButton.jsx
+++ src/components/UI/IconButton.jsx
@@ -1,7 +1,7 @@
-import { log } from '../../log.js';
-
-export default function IconButton({ children, icon, ...props }) {
-  log('<IconButton /> rendered', 2);
+import { log } from "../../log.js";
+import { memo } from "react";
+const IconButton = memo(function IconButton({ children, icon, ...props }) {
+  log("<IconButton /> rendered", 2);
 
   const Icon = icon;
   return (
@@ -10,4 +10,6 @@ export default function IconButton({ children, icon, ...props }) {
       <span className="button-text">{children}</span>
     </button>
   );
-}
+});
+
+export default IconButton;
